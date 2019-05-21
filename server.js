b03a616a54155ccff12a95e1b8bd2b49ee4ae1bc
lyss-js <adurante95@gmail.com>
2019-05-21 15:39:10 -0400

    user fields

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
 
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
