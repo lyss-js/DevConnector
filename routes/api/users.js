5b4f1f3ed5ffb565434c63d694ca18578563ffd2
lyss-js <adurante95@gmail.com>
2019-05-22 15:00:41 -0400

    authorized routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
 
-// @route    POST api/users
-// @desc     Register user
-// @access   Public
+// @route       GET api/users
+// @desc        Register user
+// @access      Public
 router.post(
   "/",
   [
@@ -35,9 +35,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -53,6 +51,7 @@ router.post(
         password
       });
 
+      // Encrypt the password
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
@@ -68,6 +67,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecret"),
+        // TO DO: switch back to an hour for deploy
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
