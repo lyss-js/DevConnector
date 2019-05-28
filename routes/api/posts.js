6a996207c4c147f31726ad370f2bd1acaf071364
lyss-js <adurante95@gmail.com>
2019-05-28 13:45:48 -0400

    setting up posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
+
 // @route    POST api/posts
 // @desc     Create a post
 // @access   Private
